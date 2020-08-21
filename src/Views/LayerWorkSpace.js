1c410a8a82dc50415c73e84f3edfc40aa46ad2fb
juggal <cipherxo1@pm.me>
2020-08-22 03:16:55 +0530

    LayerTuning added to workspace

--- src/Views/LayerWorkSpace.js
+++ src/Views/LayerWorkSpace.js
@@ -3,15 +3,18 @@ import { Grid } from "@material-ui/core";
 import {} from "@material-ui/core/styles";
 
 import Layers from "../Components/Layers";
+import LayerTuning from "../Components/LayerTuning";
 import Architecture from "../Components/Architecture";
 
 export default function LayerWorkSpace() {
   return (
-    <Grid container spacing={1} direction="row" justify="space-evenly">
+    <Grid container spacing={1} direction="row" justify="space-around">
       <Grid item>
         <Layers />
       </Grid>
-      <Grid item></Grid>
+      <Grid item>
+        <LayerTuning />
+      </Grid>
       <Grid item>
         <Architecture />
       </Grid>
