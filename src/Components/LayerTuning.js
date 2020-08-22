351ed0a402665e90880ebccd4d09f1211b5993a2
juggal <cipherxo1@pm.me>
2020-08-22 20:48:04 +0530

    removed unwanted imports

--- src/Components/LayerTuning.js
+++ src/Components/LayerTuning.js
@@ -1,25 +1,14 @@
-import React, { useState } from "react";
-import { ListItem, Slider, Select, MenuItem } from "@material-ui/core";
-import {} from "@material-ui/icons";
-import { makeStyles } from "@material-ui/core/styles";
+import React from "react";
 
 import BaseMenu from "./BaseMenu";
 import DenseLayer from "./DenseLayer";
-
-// add style object to component
-const useStyles = makeStyles((theme) => ({
-  listItmePadding: {},
-}));
+import DropoutLayer from "./DropoutLayer";
 
 export default function LayerTuning() {
-  const classes = useStyles();
-  const [age, setAge] = useState("");
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
   return (
     <BaseMenu heading="Layer Tuning">
-      <DenseLayer />
+      {/* <DenseLayer /> */}
+      <DropoutLayer />
     </BaseMenu>
   );
 }
