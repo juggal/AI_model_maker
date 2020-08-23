3d2771f4f6285a6e6159bde23d72726c60e2ea75
juggal <cipherxo1@pm.me>
2020-08-24 00:54:41 +0530

    shape componenet state added

--- src/Components/Shape.js
+++ src/Components/Shape.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
 
 export default function Shape(props) {
+  const [shape, setShape] = useState("");
   return (
     <div>
-      <TextField variant="outlined" label={props.label} required />
+      <TextField
+        variant="outlined"
+        label={props.label}
+        onChange={(e) => setShape(e.target.value)}
+        required
+      />
     </div>
   );
 }
