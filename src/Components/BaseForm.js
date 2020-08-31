174db5750560f89f0dee4b7b95fbaf7edfe0de0a
juggal <cipherxo1@pm.me>
2020-09-01 00:18:07 +0530

    added layer name to base form component

--- src/Components/BaseForm.js
+++ src/Components/BaseForm.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
 
 // add style object to the component
 const useStyles = makeStyles((theme) => ({
@@ -14,9 +16,33 @@ const useStyles = makeStyles((theme) => ({
     },
     padding: theme.spacing(1, 1.5),
   },
+  layerName: {
+    padding: theme.spacing(1, 0),
+  },
 }));
 
-export default function BaseForm(props) {
+function BaseForm({ layerName, children }) {
   const classes = useStyles();
-  return <form className={classes.root}>{props.children}</form>;
+  return (
+    <form className={classes.root}>
+      <Typography
+        variant="subtitle2"
+        color="textSecondary"
+        align="left"
+        gutterBottom
+        className={classes.layerName}
+      >
+        {layerName}
+      </Typography>
+      {children}
+    </form>
+  );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    layerName: state.layerSettings.selectedLayer.layer_name,
+  };
+};
+
+export default connect(mapStateToProps)(BaseForm);
